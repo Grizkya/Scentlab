7650e95b5d7522699505da48f467c216177ca978
akifazahratunnisah-code <akifazahratunnisah@gmail.com>
2026-06-22 20:39:11 +0800

    Update scentlab.cpp

--- scentlab.cpp
+++ scentlab.cpp
@@ -50,11 +50,6 @@ struct ResepGaleri {
     int      indeks_bahan;
 };
 
-void setGudang(BibitParfum &b, int id, const string &nama, const string &aroma, int stok, int harga) {
-    b.id = id; b.nama = nama; b.aroma = aroma;
-    b.stok_ml = stok; b.harga_per_ml = harga; b.aktif = true;
-}
-
 void garis(int panjang = 75, char karakter = '=') {
     for (int i = 0; i < panjang; i++) cout << karakter;
     cout << "\n";
@@ -96,6 +91,47 @@ void cetakBarisKosong(int jumlah = 1) {
         cout << "|";
         garis2(73, ' ');
         cout << "|\n";
+    }
+}
+
+//apri
+bool bacaInt(int &hasil, const string &pesan, int minVal, int maxVal) {
+    while (true) {
+        cout << "| " << pesan;
+        cin >> hasil;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "|   [!] Input tidak valid. Harap masukkan angka.\n";
+            continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        if (hasil < minVal || hasil > maxVal) {
+            cout << "|   [!] Angka harus di antara " << minVal << " dan " << maxVal << ".\n";
+            continue;
+        }
+        return true;
+    }
+}
+
+string bacaString(const string &pesan) {
+    string hasil;
+    while (true) {
+        cout << "| " << pesan;
+        getline(cin, hasil);
+
+        bool kosong = true;
+        for (size_t i = 0; i < hasil.length(); i++) {
+            if (hasil[i] != ' ' && hasil[i] != '\t') {
+                kosong = false;
+                break;
+            }
+        }
+        if (kosong) {
+            cout << "|   [!] Input tidak boleh kosong.\n";
+            continue;
+        }
+        return hasil;
     }
 }
 
@@ -209,6 +245,20 @@ bool pilihYaTidak(const string &pesan) {
     return (hasil == 0);
 }
 
+//ozan
+int pilihUkuranBotol() {
+    string opsi[3] = { "Botol Kecil  (30 ml)", "Botol Sedang (50 ml)", "Botol Besar  (100 ml)" };
+    int hasil = pilihMenu(opsi, 3, "KONFIGURASI UKURAN BOTOL", false, false);
+    if (hasil == 0) return 30;
+    else if (hasil == 2) return 100;
+    else return 50;
+}
+
+void setGudang(BibitParfum &b, int id, const string &nama, const string &aroma, int stok, int harga) {
+    b.id = id; b.nama = nama; b.aroma = aroma;
+    b.stok_ml = stok; b.harga_per_ml = harga; b.aktif = true;
+}
+
 void inisialisasiGudang(BibitParfum gudang[], int *jml_gudang) {
     setGudang(gudang[0],  1,  "Bergamot",      "Fruity",   500, 2000);
     setGudang(gudang[1],  2,  "Sweet Pine",    "Fruity",   300, 1500);
@@ -250,124 +300,6 @@ void inisialisasiGaleri(ResepGaleri galeri[], int *jml_galeri) {
     galeri[1].total_harga    = (15 * 3000) + (20 * 2500) + (15 * 4500);
 
     *jml_galeri = 2;
-}
-//apri
-bool bacaInt(int &hasil, const string &pesan, int minVal, int maxVal) {
-    while (true) {
-        cout << "| " << pesan;
-        cin >> hasil;
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "|   [!] Input tidak valid. Harap masukkan angka.\n";
-            continue;
-        }
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        if (hasil < minVal || hasil > maxVal) {
-            cout << "|   [!] Angka harus di antara " << minVal << " dan " << maxVal << ".\n";
-            continue;
-        }
-        return true;
-    }
-}
-
-string bacaString(const string &pesan) {
-    string hasil;
-    while (true) {
-        cout << "| " << pesan;
-        getline(cin, hasil);
-
-        bool kosong = true;
-        for (size_t i = 0; i < hasil.length(); i++) {
-            if (hasil[i] != ' ' && hasil[i] != '\t') {
-                kosong = false;
-                break;
-            }
-        }
-        if (kosong) {
-            cout << "|   [!] Input tidak boleh kosong.\n";
-            continue;
-        }
-        return hasil;
-    }
-}
-
-//ozan
-int pilihUkuranBotol() {
-    string opsi[3] = { "Botol Kecil  (30 ml)", "Botol Sedang (50 ml)", "Botol Besar  (100 ml)" };
-    int hasil = pilihMenu(opsi, 3, "KONFIGURASI UKURAN BOTOL", false, false);
-    if (hasil == 0) return 30;
-    else if (hasil == 2) return 100;
-    else return 50;
-}
-
-void menuRacik(BibitParfum gudang[], int jml_gudang, ResepGaleri galeri[], int *jml_galeri) {
-    if (*jml_galeri >= MAX_GALERI) {
-        cetakHeaderKonten("MEJA RACIK");
-        sentences("[!] Galeri sudah penuh!", RED);
-        garis(75, '=');
-        cin.get();
-        return;
-    }
-
-    ResepGaleri botolBaru;
-    botolBaru.kapasitas_maks = pilihUkuranBotol();
-    botolBaru.total_volume = 0;
-    botolBaru.total_harga  = 0;
-    botolBaru.indeks_bahan = 0;
-
-    bool tambahLagi = true;
-    do {
-        cetakHeaderKonten("MEJA RACIK - PROSES PERACIKAN");
-        cout << "|   Status Botol: " << botolBaru.total_volume << " / " << botolBaru.kapasitas_maks<< " ml | Modal: Rp " << botolBaru.total_harga << "\n";
-        cout << "|"; garis2(73, '='); cout << "|\n";
-        tampilGudang(gudang, jml_gudang, false);
-
-        int id_pilihan, takaran;
-        bacaInt(id_pilihan, "ID Bahan yang ingin dimasukkan: ", 1, jml_gudang);
-        bacaInt(takaran,    "Takaran Bahan (ml)            : ", 1, botolBaru.kapasitas_maks);
-
-        int found = -1;
-        for (int i = 0; i < jml_gudang; i++) {
-            if (gudang[i].id == id_pilihan) { found = i; break; }
-        }
-        if (found == -1) cout << "|   [!] ID tidak valid.\n";
-        else tambahBahan(&botolBaru, &gudang[found], takaran);
-
-        if (botolBaru.total_volume >= botolBaru.kapasitas_maks) {
-            cout << "|   [!] Botol racikan telah penuh.\n";
-            break;
-        }
-
-        cout << "| \n|   Tekan [ENTER] untuk memproses...";
-        cin.get();
-        tambahLagi = pilihYaTidak("Tambah jenis bibit aroma lagi?");
-    } while (tambahLagi && botolBaru.total_volume < botolBaru.kapasitas_maks);
-
-    if (botolBaru.indeks_bahan == 0) return;
-
-    bool beriNama = pilihYaTidak("Beri nama kustom/manual pada resep ini?");
-    if (beriNama) {
-        cetakHeaderKonten("BERI NAMA RACIKAN");
-        botolBaru.nama_parfum = bacaString("Nama Parfum Kustom: ");
-        cout << "| Deskripsi Vibes   : ";
-        getline(cin, botolBaru.vibes);
-    } else {
-        autoNaming(&botolBaru);
-    }
-
-    if (pilihYaTidak("Simpan mahakarya racikan ini ke Galeri?")) {
-        galeri[*jml_galeri] = botolBaru;
-        (*jml_galeri)++;
-        cetakHeaderKonten("BERHASIL");
-        sentences("[+] Resep Berhasil Diamankan ke Galeri!", GREEN);
-    } else {
-        cetakHeaderKonten("BATAL");
-        sentences("[X] Formula dibuang. Sisa stok bahan tidak kembali.", RED);
-    }
-    garis(75, '=');
-    cout << "|   Tekan [ENTER] untuk kembali ke Menu Utama...";
-    cin.get();
 }
 
 // rasyad
@@ -472,6 +404,7 @@ void tambahBahan(ResepGaleri *botol, BibitParfum *bahan, int takaran) {
     botol->indeks_bahan++;
     cout << "|   [+] BERHASIL: " << takaran << " ml " << bahan->nama << " ditambahkan.\n";
 }
+
 //kifa
 void autoNaming(ResepGaleri *botol) {
     if (botol->indeks_bahan > 0) {
@@ -482,8 +415,75 @@ void autoNaming(ResepGaleri *botol) {
         botol->vibes       = "-";
     }
 }
-//ozan
 
+void menuRacik(BibitParfum gudang[], int jml_gudang, ResepGaleri galeri[], int *jml_galeri) {
+    if (*jml_galeri >= MAX_GALERI) {
+        cetakHeaderKonten("MEJA RACIK");
+        sentences("[!] Galeri sudah penuh!", RED);
+        garis(75, '=');
+        cin.get();
+        return;
+    }
+
+    ResepGaleri botolBaru;
+    botolBaru.kapasitas_maks = pilihUkuranBotol();
+    botolBaru.total_volume = 0;
+    botolBaru.total_harga  = 0;
+    botolBaru.indeks_bahan = 0;
+
+    bool tambahLagi = true;
+    do {
+        cetakHeaderKonten("MEJA RACIK - PROSES PERACIKAN");
+        cout << "|   Status Botol: " << botolBaru.total_volume << " / " << botolBaru.kapasitas_maks<< " ml | Modal: Rp " << botolBaru.total_harga << "\n";
+        cout << "|"; garis2(73, '='); cout << "|\n";
+        tampilGudang(gudang, jml_gudang, false);
+
+        int id_pilihan, takaran;
+        bacaInt(id_pilihan, "ID Bahan yang ingin dimasukkan: ", 1, jml_gudang);
+        bacaInt(takaran,    "Takaran Bahan (ml)            : ", 1, botolBaru.kapasitas_maks);
+
+        int found = -1;
+        for (int i = 0; i < jml_gudang; i++) {
+            if (gudang[i].id == id_pilihan) { found = i; break; }
+        }
+        if (found == -1) cout << "|   [!] ID tidak valid.\n";
+        else tambahBahan(&botolBaru, &gudang[found], takaran);
+
+        if (botolBaru.total_volume >= botolBaru.kapasitas_maks) {
+            cout << "|   [!] Botol racikan telah penuh.\n";
+            break;
+        }
+
+        cout << "| \n|   Tekan [ENTER] untuk memproses...";
+        cin.get();
+        tambahLagi = pilihYaTidak("Tambah jenis bibit aroma lagi?");
+    } while (tambahLagi && botolBaru.total_volume < botolBaru.kapasitas_maks);
+
+    if (botolBaru.indeks_bahan == 0) return;
+
+    bool beriNama = pilihYaTidak("Beri nama kustom/manual pada resep ini?");
+    if (beriNama) {
+        cetakHeaderKonten("BERI NAMA RACIKAN");
+        botolBaru.nama_parfum = bacaString("Nama Parfum Kustom: ");
+        cout << "| Deskripsi Vibes   : ";
+        getline(cin, botolBaru.vibes);
+    } else {
+        autoNaming(&botolBaru);
+    }
+
+    if (pilihYaTidak("Simpan mahakarya racikan ini ke Galeri?")) {
+        galeri[*jml_galeri] = botolBaru;
+        (*jml_galeri)++;
+        cetakHeaderKonten("BERHASIL");
+        sentences("[+] Resep Berhasil Diamankan ke Galeri!", GREEN);
+    } else {
+        cetakHeaderKonten("BATAL");
+        sentences("[X] Formula dibuang. Sisa stok bahan tidak kembali.", RED);
+    }
+    garis(75, '=');
+    cout << "|   Tekan [ENTER] untuk kembali ke Menu Utama...";
+    cin.get();
+}
 
 //kifa
 void cetakGaleri(ResepGaleri galeri[], int jml_galeri) {
@@ -518,7 +518,6 @@ void cetakGaleri(ResepGaleri galeri[], int jml_galeri) {
             cout << "|"; garis2(73, '-'); cout << "|\n";
         }
     }
-
     garis(75, '=');
     cout << "|   Tekan [ENTER] untuk kembali ke Menu Utama...";
     cin.get();
