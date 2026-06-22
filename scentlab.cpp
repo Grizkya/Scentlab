b7d01231ca1c902f696c3c4612d44106d1fb8210
Grizkya <ghaitsarizkyamalia@gmail.com>
2026-06-22 16:08:12 +0800

    Initialize scentlab.cpp with basic setup

---
+++ scentlab.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <limits>
+#include <conio.h>
+#include <sstream>
+
+using namespace std;
+
+// ANSI Escape Code
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define BLUE    "\033[34m"
+#define YELLOW  "\033[33m"
+#define RESET   "\033[0m"
+#define CLS cout << "\033[2J\033[H" << flush
+
+// Konstanta Global
+const int MAX_GUDANG     = 20;
+const int MAX_GALERI     = 50;
+const int MAX_KOMPOSISI  = 15;
+const int KEY_PREFIX_WIN = 224;
+const int KEY_PREFIX_ALT = 0;
+const int KEY_UP         = 72;
+const int KEY_DOWN       = 80;
+const int KEY_ENTER      = 13;
+
+int main()
+{
+
+}
